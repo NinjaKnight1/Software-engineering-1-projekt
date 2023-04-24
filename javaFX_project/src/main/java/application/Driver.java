f9586a08cbaf6463b3e67ea0f79ab721defab5dd
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-04-24 18:41:20 +0200

    Update driver with new label

--- javaFX_project/src/main/java/application/Driver.java
+++ javaFX_project/src/main/java/application/Driver.java
@@ -1,5 +1,27 @@
 package application;
 
-public class Driver {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+
+
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("Indtast dit brugernavn her");
+
+
+        //Label l = new Label("Lav en ny bruger");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
